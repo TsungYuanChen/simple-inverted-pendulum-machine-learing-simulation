19d41af98a40e073f9fca5aba1a41d9c6cdcfe16
Tsung-Yuan Chen <wainwirt@gmail.com>
2025-06-06 18:55:24 +0900

    Update cartpole-learn-agent1.py
    Clear the miss spaces

--- cartpole-learn-agent1.py
+++ cartpole-learn-agent1.py
@@ -14,13 +14,12 @@ model.save("ppo_cartpole_model")
 state, _ = env.reset()
 
 for _ in range(2000):
-    env.render()
-
-    action, _ = model.predict(state, deterministic=True)
-    state, reward, terminated, truncated, info = env.step(action)
-
-    if terminated or truncated:
-        state, _ = env.reset()
-
+    env.render()
+    action, _ = model.predict(state, deterministic=True)
+    state, reward, terminated, truncated, info = env.step(action)
+    
+    if terminated or truncated:
+       state, _ = env.reset()
+      
 # 環境を終了
 env.close()
